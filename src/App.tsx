91badd99dd884ea5a5d250709deec83b534ad61b
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-10-21 22:59:28 -0400

    add firebase configuration

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Journey Map</h1>
+
     </>
   );
 }
---
+++ src/config/firebase-config.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
