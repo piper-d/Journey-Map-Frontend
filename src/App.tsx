27d7691495973a7959f6d8a12649f682af625c80
Dylan Piper <Dylan_piper@knights.ucf.edu>
2023-01-31 21:26:01 -0500

    Dark mode!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider, CssBaseline, PaletteMode, createTheme } from '@mui/material';
+
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,19 +16,19 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 
+
+
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [mode, setMode] = React.useState<PaletteMode>(prefersDarkMode ? "dark" : "light");
+  const [mode, setMode] = useLocalStorage<string>("mode", prefersDarkMode ? "dark" : "light");
 
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) => 
-          prevMode === "light" ? "dark" : "light"
-        );
+        setMode(mode === "light" ? "dark" : "light");
       },
     }),
-    []
+    [mode]
   )
 
   const theme = React.useMemo(
@@ -51,10 +52,46 @@ function App() {
         </BrowserRouter>
       </ThemeProvider>
     </ColorContext.Provider>
-
-    
-
   )
+}
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue] as const;
 }
 
 export default App;
