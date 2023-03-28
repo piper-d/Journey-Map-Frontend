ca0b5c36e188c982801e138c5ef182e28b8a2f4a
Dylan Piper <Dylan_piper@knights.ucf.edu>
2023-03-28 17:19:14 -0400

    Metadata extraction

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,12 +1,57 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Button, TextField } from '@mui/material';
+import loadImage from 'blueimp-load-image';
+
+import EXIF from 'exif-js';
+
+import exifr from 'exifr'
 
 
 const ImageUploader: React.FunctionComponent<any> = (tripId) => {
     const [selectedImage, setSelectedImage] = useState<File | null>(null);
     const [location, setLocation] = useState<{ lat?: number; lng?: number }>({});
-    const token = sessionStorage.getItem("accessToken")
+    const token = sessionStorage.getItem("accessToken");
+
+    const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        console.log(files)
+        if (files && files.length > 0) {
+            setSelectedImage(files[0]);
+
+            exifr.gps(files[0]).then(gps => {
+                setLocation({ lat: gps.latitude, lng: gps.longitude })
+            })
+        }
+    };
+
+    const handleSubmit = () => {
+        try {
+            if (!selectedImage) return;
+            const extension = selectedImage.name.split('.').pop();
+
+            const data = {
+                latitude: location.lat?.toString() ?? '',
+                longitude: location.lng?.toString() ?? '',
+                image: selectedImage,
+                extension: extension ?? ''
+            }
+            PutUserData(token, data, tripId.tripId)
+
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setLocation(prevLocation => ({
+            ...prevLocation,
+            lat: name === 'lat' ? Number(value) : prevLocation?.lat || undefined,
+            lng: name === 'lng' ? Number(value) : prevLocation?.lng || undefined
+        }));
+    };
 
     const PutUserData = async (token: any, data: any, tripId: string) => {
         try {
@@ -28,41 +73,6 @@ const ImageUploader: React.FunctionComponent<any> = (tripId) => {
         }
     }
 
-    const handleSubmit = () => {
-        try {
-            if (!selectedImage) return;
-            const extension = selectedImage.name.split('.').pop();
-
-            const data = {
-                latitude: location.lat?.toString() ?? '',
-                longitude: location.lng?.toString() ?? '',
-                image: selectedImage,
-                extension: extension ?? ''
-            }
-            PutUserData(token, data, tripId.tripId)
-
-        } catch (error) {
-            console.error(error);
-        }
-
-    }
-
-    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files && files.length > 0) {
-            setSelectedImage(files[0]);
-        }
-    };
-
-    const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-        setLocation(prevLocation => ({
-            ...prevLocation,
-            lat: name === 'lat' ? Number(value) : prevLocation?.lat || undefined,
-            lng: name === 'lng' ? Number(value) : prevLocation?.lng || undefined
-        }));
-    };
-
     return (
         <div>
             <input type="file" onChange={handleImageChange} />
@@ -72,6 +82,8 @@ const ImageUploader: React.FunctionComponent<any> = (tripId) => {
                 label="Latitude"
                 type="number"
                 value={location.lat || ''}
+
+
                 name="lat"
                 onChange={handleLocationChange}
             />
